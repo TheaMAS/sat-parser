e1e83bfe0b71e43656a70ecd5da20415778abb1a
Robert Cardona <robertcardona@github.com>
2023-04-05 21:05:38 -0400

    Fixed set datatype naming conflict.

--- file_parser.py
+++ file_parser.py
@@ -1,4 +1,5 @@
-import contact_analysis as ca
+# import contact_analysis as ca
+import report_parser as rp
 import portion as P
 
 from matrix import IntervalMatrix
@@ -6,7 +7,8 @@ from matrix import IntervalMatrix
 # file_parser.py
 # Contains functions which handle data from files
 
-
+# TODO : RC thinks this should be moved into report_parser since it deals
+#	with the contact analysis reports
 
 
 # soap_converter
@@ -16,12 +18,14 @@ from matrix import IntervalMatrix
 # @return: IntervalMatrix
 
 def soap_converter(contactsheet):
-    contact_plan = ca.contact_analysis_parser(contactsheet)
-    graph = ca.construct_graph(contact_plan)
+    contact_plan = rp.contact_analysis_parser(contactsheet)
+    graph = rp.construct_graph(contact_plan)
+
     nodes = graph["nodes"]
     edges = graph["edges"]
 
-    node_counter = len(nodes);
+    node_counter = len(nodes)
+    print(node_counter)
 
     #generate blank matrix for overall
     Final=[[ P.empty() for i in range(node_counter) ] for j in range(node_counter)]
@@ -36,14 +40,136 @@ def soap_converter(contactsheet):
         # print(len(edge_times))
 
         for i in range(0, len(edge_times), 2):
-            rise = edge_times[i]
-            set = edge_times[i + 1]
+            rise_time = edge_times[i]
+            set_time = edge_times[i + 1]
 
             # rise = edge_times[0]
             # set = edge_times[1]
 
             #add interval to both F[i][j] and F[j][i] via union (should create disjoint unions generally)
-            Final[nodes[source]][nodes[destination]] = Final[nodes[source]][nodes[destination]] | P.open(rise, set)
-            Final[nodes[destination]][nodes[source]] = Final[nodes[destination]][nodes[source]] | P.open(rise, set)
+            Final[nodes[source]][nodes[destination]] = Final[nodes[source]][nodes[destination]] | P.open(rise_time, set_time)
+            Final[nodes[destination]][nodes[source]] = Final[nodes[destination]][nodes[source]] | P.open(rise_time, set_time)
 
     return IntervalMatrix(node_counter, node_counter, Final)
+
+if __name__ == "__main__":
+
+	A = soap_converter("./outputs/starlink-15-sat-single/starlink_15_sats_0 Contact Analysis.csv")
+
+	test = """
+		"STARLINK-1199 - STARLINK-2419"
+		"STARLINK-1199 - STARLINK-2451"
+		"STARLINK-1199 - STARLINK-3633"
+		"STARLINK-1199 - STARLINK-3740"
+		"STARLINK-1199 - STARLINK-2228"
+		"STARLINK-1199 - STARLINK-3606"
+		"STARLINK-1199 - STARLINK-2151"
+		"STARLINK-1199 - STARLINK-3516"
+		"STARLINK-1199 - STARLINK-3818"
+		"STARLINK-1199 - STARLINK-2082"
+		"STARLINK-1199 - STARLINK-1849"
+		"STARLINK-1199 - STARLINK-1054"
+		"STARLINK-1199 - STARLINK-1882"
+		"STARLINK-1199 - STARLINK-1680"
+		"STARLINK-2419 - STARLINK-2451"
+		"STARLINK-2419 - STARLINK-3633"
+		"STARLINK-2419 - STARLINK-3740"
+		"STARLINK-2419 - STARLINK-2228"
+		"STARLINK-2419 - STARLINK-3606"
+		"STARLINK-2419 - STARLINK-2151"
+		"STARLINK-2419 - STARLINK-3516"
+		"STARLINK-2419 - STARLINK-3818"
+		"STARLINK-2419 - STARLINK-2082"
+		"STARLINK-2419 - STARLINK-1849"
+		"STARLINK-2419 - STARLINK-1054"
+		"STARLINK-2419 - STARLINK-1882"
+		"STARLINK-2419 - STARLINK-1680"
+		"STARLINK-2451 - STARLINK-3633"
+		"STARLINK-2451 - STARLINK-3740"
+		"STARLINK-2451 - STARLINK-2228"
+		"STARLINK-2451 - STARLINK-3606"
+		"STARLINK-2451 - STARLINK-2151"
+		"STARLINK-2451 - STARLINK-3516"
+		"STARLINK-2451 - STARLINK-3818"
+		"STARLINK-2451 - STARLINK-2082"
+		"STARLINK-2451 - STARLINK-1849"
+		"STARLINK-2451 - STARLINK-1054"
+		"STARLINK-2451 - STARLINK-1882"
+		"STARLINK-2451 - STARLINK-1680"
+		"STARLINK-3633 - STARLINK-3740"
+		"STARLINK-3633 - STARLINK-2228"
+		"STARLINK-3633 - STARLINK-3606"
+		"STARLINK-3633 - STARLINK-2151"
+		"STARLINK-3633 - STARLINK-3516"
+		"STARLINK-3633 - STARLINK-3818"
+		"STARLINK-3633 - STARLINK-2082"
+		"STARLINK-3633 - STARLINK-1849"
+		"STARLINK-3633 - STARLINK-1054"
+		"STARLINK-3633 - STARLINK-1882"
+		"STARLINK-3633 - STARLINK-1680"
+		"STARLINK-3740 - STARLINK-2228"
+		"STARLINK-3740 - STARLINK-3606"
+		"STARLINK-3740 - STARLINK-2151"
+		"STARLINK-3740 - STARLINK-3516"
+		"STARLINK-3740 - STARLINK-3818"
+		"STARLINK-3740 - STARLINK-2082"
+		"STARLINK-3740 - STARLINK-1849"
+		"STARLINK-3740 - STARLINK-1054"
+		"STARLINK-3740 - STARLINK-1882"
+		"STARLINK-3740 - STARLINK-1680"
+		"STARLINK-2228 - STARLINK-3606"
+		"STARLINK-2228 - STARLINK-2151"
+		"STARLINK-2228 - STARLINK-3516"
+		"STARLINK-2228 - STARLINK-3818"
+		"STARLINK-2228 - STARLINK-2082"
+		"STARLINK-2228 - STARLINK-1849"
+		"STARLINK-2228 - STARLINK-1054"
+		"STARLINK-2228 - STARLINK-1882"
+		"STARLINK-2228 - STARLINK-1680"
+		"STARLINK-3606 - STARLINK-2151"
+		"STARLINK-3606 - STARLINK-3516"
+		"STARLINK-3606 - STARLINK-3818"
+		"STARLINK-3606 - STARLINK-2082"
+		"STARLINK-3606 - STARLINK-1849"
+		"STARLINK-3606 - STARLINK-1054"
+		"STARLINK-3606 - STARLINK-1882"
+		"STARLINK-3606 - STARLINK-1680"
+		"STARLINK-2151 - STARLINK-3516"
+		"STARLINK-2151 - STARLINK-3818"
+		"STARLINK-2151 - STARLINK-2082"
+		"STARLINK-2151 - STARLINK-1849"
+		"STARLINK-2151 - STARLINK-1054"
+		"STARLINK-2151 - STARLINK-1882"
+		"STARLINK-2151 - STARLINK-1680"
+		"STARLINK-3516 - STARLINK-3818"
+		"STARLINK-3516 - STARLINK-2082"
+		"STARLINK-3516 - STARLINK-1849"
+		"STARLINK-3516 - STARLINK-1054"
+		"STARLINK-3516 - STARLINK-1882"
+		"STARLINK-3516 - STARLINK-1680"
+		"STARLINK-3818 - STARLINK-2082"
+		"STARLINK-3818 - STARLINK-1849"
+		"STARLINK-3818 - STARLINK-1054"
+		"STARLINK-3818 - STARLINK-1882"
+		"STARLINK-3818 - STARLINK-1680"
+		"STARLINK-2082 - STARLINK-1849"
+		"STARLINK-2082 - STARLINK-1054"
+		"STARLINK-2082 - STARLINK-1882"
+		"STARLINK-2082 - STARLINK-1680"
+		"STARLINK-1849 - STARLINK-1054"
+		"STARLINK-1849 - STARLINK-1882"
+		"STARLINK-1849 - STARLINK-1680"
+		"STARLINK-1054 - STARLINK-1882"
+		"STARLINK-1054 - STARLINK-1680"
+		"STARLINK-1882 - STARLINK-1680"
+	"""
+
+
+	sats = set()
+	test = test.replace("\t ", "").replace("\"", "").split("\n")
+	for pair in test[1:-1]:
+		source, destination = pair.split(" - ")
+			
+		sats.add(source)
+		sats.add(destination)
+	print(sats)
